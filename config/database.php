6f500388edd8af2524bcaa9810b16ffcc080c86e
Sevan74 <sonzini.evan2005@gmail.com>
2025-06-16 13:19:54 +0200

    database

--- config/database.php
+++ config/database.php
@@ -6,7 +6,7 @@ $username = 'user';
 $password = 'rQUSxP2xUCxnzU45';     
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo = new PDO('mysql:host=mysql-ijtebowmandevill.mysql.db', 'user', 'pass');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch(PDOException $e) {
